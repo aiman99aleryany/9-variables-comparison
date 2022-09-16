641c3d4f394b19d587cbae4e3d848e65d0fe54fe
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 15:09:46 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,20 @@
+// # Exercise 9
+
+// * Try to predict the output of the comparisons before printing them in the console
+
+const name = "Mario";
+const lastName = "Rossi";
+const age = 27;
+const isGraduated = false;
+const average = 27;
+const examsCompleted = 10;
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+name == lastName; // false
+age <= average; // true
+firstYearCompleted == lastName; // I predicted true, but somehow it's false.
+yearsCompleted == firstYearCompleted; // true
+firstYearCompleted === yearsCompleted; // false
+examsCompleted < age; // true
+isGraduated > yearsCompleted; // false
